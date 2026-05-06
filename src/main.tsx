af48e8bd4e0fc7091fd99fe1ef15f1717c9e2669
youssefalw2001 <youssefalw2001@gmail.com>
2026-05-06 20:51:42 +0300

    Add premium First Set Lab intro
    Add a premium First Set Lab home briefing and use BrowserRouter for Telegram compatibility.

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
-const legacyPath = window.location.pathname;
-const hasHashRoute = window.location.hash.startsWith('#/');
-
-if (legacyPath !== '/' && !hasHashRoute) {
-  window.history.replaceState(null, '', `/#${legacyPath}${window.location.search}`);
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
