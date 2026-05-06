b49d533ff80d27c451e395642672b50b4cba0bbc
youssefalw2001 <youssefalw2001@gmail.com>
2026-05-06 20:03:20 +0300

    Normalize legacy static routes to hash routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
+const legacyPath = window.location.pathname;
+const hasHashRoute = window.location.hash.startsWith('#/');
+
+if (legacyPath !== '/' && !hasHashRoute) {
+  window.history.replaceState(null, '', `/#${legacyPath}${window.location.search}`);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
