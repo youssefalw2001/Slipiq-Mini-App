c84318a80de30c0b57db642e84e0327108131e24
youssefalw2001 <youssefalw2001@gmail.com>
2026-05-20 21:36:08 +0300

    Load checkout styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import './styles.css';
 import './iq-rebuild.css';
 import './score-hunter.css';
+import './checkout.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
